628312ec39e584c262a071c1caf94efe33c508f3
Tim Lightfoot <tlightfo@gmail.com>
2014-02-20 14:51:13 -0600

    Added base project files.
    Here are the default "new project" files. We will build of this!

---
+++ StudentPortal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentPortal::Application.config.secret_token = '3527d7eb312d30413e892a39c55d95b9bb840ae62092bc0a18be748844cb95a07bd817ffec825596d205db0d7325b84cd7e60f6c403150336649e837c609e209'
