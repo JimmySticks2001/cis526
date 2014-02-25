609d1ea714625334fbd9c123a12be75305f81780
orealeb <orealeb@yahoo.com>
2014-02-25 15:12:24 -0600

    Update event.rb

--- StudentPortal/app/models/event.rb
+++ StudentPortal/app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :additionalInfo, :end, :sponsor, :start, :title
-  validates :name, length: {minimum: 0, maximum: 160}, allow_blank: true, 
+  validates :additionalInfo, length: {minimum: 0, maximum: 160}, allow_blank: true, 
   format: { with: /.*[a-zA-Z].*/, message: "must include letters" }
   #validation for addiitonal info not working  
 
