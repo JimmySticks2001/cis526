d295a4f05b983570a3c8f869e924b4931469dfc7
Timothy Lightfoot <viper.cis.ksu.edu>
2014-02-25 18:09:38 -0600

    Added validation for title field. Added tests for event model.

--- StudentPortal/app/models/event.rb
+++ StudentPortal/app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ActiveRecord::Base
   validates :additionalInfo, length: {minimum: 0, maximum: 160}, allow_blank: true, 
   format: { with: /.*[a-zA-Z].*/, message: "must include letters" }
+
+  validates :title,
+  length: {minimum: 3, maximum: 56},
+  allow_blank: false,
+  format: { with: /.*[a-zA-Z].*/, 
+  message: "minimum 3 characters, max 56, not all whitespace, and must include letters" }
+  
 end
