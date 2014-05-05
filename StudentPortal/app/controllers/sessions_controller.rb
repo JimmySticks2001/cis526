0805539ccc9f15ee81398468e56dd7c7e20e3e63
Timothy Lightfoot <tlightfo@gmail.com>
2014-05-05 11:59:57 -0500

    added invalid username or password warning.

--- StudentPortal/app/controllers/sessions_controller.rb
+++ StudentPortal/app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController  
- def authenticate(userName, password)
-  u = first(:conditions => {:userName => userName, :state => 'active'})
-  u && u.authenticated?(password) ? [true, u] : [false, u]
- end
 
-  def new  
-  end  
-     def index  
+  def authenticate(userName, password)
+    u = first(:conditions => {:userName => userName, :state => 'active'})
+    u && u.authenticated?(password) ? [true, u] : [false, u]
+  end
+
+  def new
+  end
+
+  def index
   end
 
   def create  
@@ -14,24 +16,24 @@ class SessionsController < ApplicationController
     if user  
       session[:user_id] = user.id  
       redirect_to root_url 
-    else  
-      flash.now.alert = "Invalid userName or password"  
-      render "new"  
+    else
+      redirect_to root_url, alert: "Invalid usename or password"
     end  
   end 
  
-def destroy  
-  session[:user_id] = nil  
-  redirect_to root_url
-end  
+  def destroy  
+    session[:user_id] = nil  
+    redirect_to root_url
+  end  
 
- private
+  private
 
   def password_params
     params.require(:password)
-
   end
-   def userName_params
+
+  def userName_params
     params.require(:userName)
   end
+  
 end  
