9df842ec1f9df7189ff6866cd761361f8fc3ad2f
Timothy Lightfoot <tlightfo@gmail.com>
2014-05-06 15:50:37 -0500

    fixed sticky footer position on mobile site. Fixed social media and castle image position on main site.

--- StudentPortal/app/models/research.rb
+++ StudentPortal/app/models/research.rb
@@ -1,2 +1,15 @@
 class Research < ActiveRecord::Base
+	
+	validates :title, 
+  	length: {minimum: 3, maximum: 140},
+  	allow_blank: false, 
+  	format: { with: /.*[a-zA-Z].*/,
+  	message: "Title must be between 3 and 140 characters and must include letters." }
+	
+	validates :content, 
+  	length: {minimum: 3, maximum: 1500},
+  	allow_blank: false, 
+  	format: { with: /.*[a-zA-Z].*/,
+  	message: "Content must be between 3 and 1500 characters and must include letters." }
+	
 end
