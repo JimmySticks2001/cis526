2e318f353a6ba841e50b1b0e8c5ea9a04c7abcfe
orealeb <orealeb@yahoo.com>
2014-05-06 00:54:51 -0500

    Added validation to faculty name, email, and personal webpage link

--- StudentPortal/app/models/faculty.rb
+++ StudentPortal/app/models/faculty.rb
@@ -1,5 +1,17 @@
 class Faculty < ActiveRecord::Base
 has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
+validates :name,
+  	length: {minimum: 3, maximum: 56},
+  	allow_blank: false,
+  	format: { with: /.*[a-zA-Z].*/, 
+  	message: "minimum 3 characters, max 56, not all whitespace, and must include letters" }
+validates :personal_page_link,  
+	allow_blank: true, 
+	format: { with: URI::regexp(%w(http https)),
+	message: "link must include http or https" }
+validates :email,
+	allow_blank: true,
+	format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, 
+	message: "email format invalid" }
 end
