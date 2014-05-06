3e1a6d4f80a318d1740984088f6c675b7e09c8d5
orealeb <orealeb@yahoo.com>
2014-05-06 00:54:02 -0500

    Added validation to club name and club webpage link

--- StudentPortal/app/models/club.rb
+++ StudentPortal/app/models/club.rb
@@ -1,2 +1,11 @@
 class Club < ActiveRecord::Base
+validates :name,
+  	length: {minimum: 3, maximum: 56},
+  	allow_blank: false,
+  	format: { with: /.*[a-zA-Z].*/, 
+  	message: "minimum 3 characters, max 56, not all whitespace, and must include letters" }
+validates :club_page_link,  
+	allow_blank: true, 
+	format: { with: URI::regexp(%w(http https)),
+	message: "link must include http or https" }
 end
