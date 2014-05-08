96c3b9bfae2e0bab9a57c383bca8d82254924c87
Timothy Lightfoot <tlightfo@gmail.com>
2014-05-08 15:06:30 -0500

    Fixed event no image upload default. Fixed front page events table.

--- StudentPortal/app/models/event.rb
+++ StudentPortal/app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 
-	has_attached_file :avatar, :styles => { :medium => "900x900>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "900x900>", :thumb => "100x100>" }, :default_url => ""
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/  
 
 	
